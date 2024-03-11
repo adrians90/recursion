bc719981da4e59b27524c069b1d111af88abacbc
Adrian <adriansandu9090@gmail.com>
2024-03-11 11:05:46 +0000

    added a fibonacci function that calculates the sum of fibonacci numbers up to given N and returns the value

--- project1/fibs.js
+++ project1/fibs.js
@@ -15,3 +15,13 @@ function fibs(number) {
 }
 
 console.log(fibs(8))
+
+function fibonacci(n) {
+    if (n <= 1) {
+        return n
+    }
+    return fibonacci(n - 1) + fibonacci (n - 2)
+}
+
+
+console.log(fibonacci(10))
